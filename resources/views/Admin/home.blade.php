80f19c5a1d388a1ff646176f0c5e003967b82400
Diyaneth <103946158+Diyaneth@users.noreply.github.com>
2023-11-09 23:22:52 +0530

    Integrate html

---
+++ resources/views/Admin/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+   
+</x-app-layout>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+
+    <body>
+        admin
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[HomeController::class,'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+Route::get('/redirect',[HomeController::class,'redirect']);
