233fb930aef19a6fa0cbe044909e689f1c8a96f0
Diyaneth <103946158+Diyaneth@users.noreply.github.com>
2023-11-27 11:34:47 +0530

    Update slidebar.blade.php

--- resources/views/Admin/slidebar.blade.php
+++ resources/views/Admin/slidebar.blade.php
@@ -24,21 +24,22 @@
           </li>
 
           <li class="nav-item menu-items">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <a class="nav-link" href="{{url('view_product')}}">
               <span class="menu-icon">
-                <i class="mdi mdi-laptop"></i>
+                <i class="mdi mdi-playlist-play"></i>
               </span>
-              <span class="menu-title">Products</span>
-              <i class="menu-arrow"></i>
+              <span class="menu-title">Add Product</span>
             </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="{{url('/view_product')}}">Add Products</a></li>
-                <li class="nav-item"> <a class="nav-link" href="{{url('/show_product')}}">Show Products</a></li>
-                
-              </ul>
-            </div>
           </li>
+
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('show_product')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-playlist-play"></i>
+              </span>
+              <span class="menu-title">Show Product</span>
+            </a>
+          </li> 
           
 
           <li class="nav-item menu-items">
