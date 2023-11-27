39985398af901acaa6979b8a349e2061a8a2a58d
Diyaneth <103946158+Diyaneth@users.noreply.github.com>
2023-11-27 11:23:40 +0530

    Update order.blade.php

--- resources/views/home/order.blade.php
+++ resources/views/home/order.blade.php
@@ -58,7 +58,7 @@
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class=""> </span>
                   </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <div>
                      <ul class="navbar-nav">
                         <li class="nav-item">
                            <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
